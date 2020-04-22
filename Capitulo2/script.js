227acc944037452d1774bd59db532c80c4df6c5b
Denis Romão <de.silva.romao@gmail.com>
2020-04-22 18:24:51 -0300

    Commit 1 = Iniciando Livro: JavaScript O Guia Definitivo

---
+++ Capitulo2/script.js
@@ -0,0 +1,14 @@
+// Comentário de uma linha
+/* Comentário */ // Outro comentário
+/*
+ *Comentário de multiplas linhas
+ *continua o comentário
+*/
+
+// PONTOS E VIRGULA (;) SÃO OPCIONAIS
+a = 0
+b = 1
+
+//aqui a virgula é obrigatória
+var a; a = 3; console.log(a)
+
